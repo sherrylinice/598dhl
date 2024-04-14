d69f61ca696b0a34d68b430a10eca95a43a4a840
sherrylinice <134333239+sherrylinice@users.noreply.github.com>
2024-04-13 22:56:21 -0700

    Update ablation_option.py
    added hidden_layer_removal

--- code/ablation_option.py
+++ code/ablation_option.py
@@ -1,7 +1,8 @@
 class AblationOption:
-  def __init__(self, normalization_layer_removal, max_pool):
+  def __init__(self, normalization_layer_removal, max_pool, hidden_layer_removal):
     self.normalization_layer_removal = normalization_layer_removal
     self.max_pool = max_pool
+    self.hidden_layer_removal = hidden_layer_removal
 
 #  def __init__(self, normalization_layer_removal, max_pool):
 #    self.normalization_layer_removal = normalization_layer_removal
